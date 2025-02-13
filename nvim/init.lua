cb7dbfd5b0cbe40c28c3b3f8a6f76f9b1a2ee84c
Hugo <114680022+hugorodriv@users.noreply.github.com>
2025-02-13 12:44:24 +0100

    initial dot config

---
+++ nvim/init.lua
@@ -0,0 +1,62 @@
+-- bootstrap lazy.nvim, lazyvim and your plugins
+
+require("config.lazy")
+require("config.lualine")
+require("lint").linters_by_ft = {
+    bash = { "shellcheck" },
+    sh = { "shellcheck" },
+}
+
+vim.g.snacks_animate = false --Disable animations globally
+
+-- Colorscheme
+vim.cmd("colorscheme kanagawa")
+
+-- For some reason the ctrl-jk keys only work when "source $MYVIMRC" command is run. This is a janky way of doing it automatically
+-- not directly sourcing $MYVIMRC because       1) it's probably stupid       2) kinda breaks the colorscheme (probably bc it's stupid)
+vim.api.nvim_create_autocmd("BufAdd", { command = "source ~/.config/nvim/lua/config/lineshifter.lua" })
+
+-- 4 space identation
+vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
+vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
+vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
+
+-- Ctr U/D page cenetring
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Center cursor after moving down half-page" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Center cursor after moving down half-page" })
+vim.keymap.set("n", "G", "Gzz", { desc = "Center cursor after moving down half-page" })
+
+-- YankyPut
+vim.keymap.set({ "n", "x" }, "p", "<Plug>(YankyPutAfter)")
+vim.keymap.set({ "n", "x" }, "P", "<Plug>(YankyPutBefore)")
+vim.keymap.set({ "n", "x" }, "gp", "<Plug>(YankyGPutAfter)")
+vim.keymap.set({ "n", "x" }, "gP", "<Plug>(YankyGPutBefore)")
+vim.keymap.set("n", "<c-p>", "<Plug>(YankyPreviousEntry)")
+vim.keymap.set("n", "<c-n>", "<Plug>(YankyNextEntry)")
+
+-- editor.dial
+vim.keymap.set("n", "<C-a>", function()
+    require("dial.map").manipulate("increment", "normal")
+end)
+vim.keymap.set("n", "<C-x>", function()
+    require("dial.map").manipulate("decrement", "normal")
+end)
+vim.keymap.set("n", "g<C-a>", function()
+    require("dial.map").manipulate("increment", "gnormal")
+end)
+vim.keymap.set("n", "g<C-x>", function()
+    require("dial.map").manipulate("decrement", "gnormal")
+end)
+vim.keymap.set("v", "<C-a>", function()
+    require("dial.map").manipulate("increment", "visual")
+end)
+vim.keymap.set("v", "<C-x>", function()
+    require("dial.map").manipulate("decrement", "visual")
+end)
+vim.keymap.set("v", "g<C-a>", function()
+    require("dial.map").manipulate("increment", "gvisual")
+end)
+vim.keymap.set("v", "g<C-x>", function()
+    require("dial.map").manipulate("decrement", "gvisual")
+end)
