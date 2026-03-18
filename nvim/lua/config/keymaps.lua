421af30f552bad177ccf95895326e3579c1216ee
Hugo Rodriguez <114680022+hugorodriv@users.noreply.github.com>
2026-03-18 08:44:56 +0100

    solve recursive bufadd command

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Override LazyVim's default <C-j>/<C-k> window navigation with line move mappings.
+require("keymaps.move-line-ctrl-j-k")
--- nvim/lua/keymaps/move-line-ctrl-j-k.lua
+++ nvim/lua/keymaps/move-line-ctrl-j-k.lua
@@ -1,6 +1,3 @@
--- For some reason the ctrl-jk keys only work when "source $MYVIMRC" command is run
-vim.api.nvim_create_autocmd("BufAdd", { command = "source ~/.config/nvim/lua/keymaps/move-line-ctrl-j-k.lua" })
-
 vim.keymap.set("n", "<C-k>", ":m .-2<CR>==", { desc = "Move line up", silent = true })
 vim.keymap.set("n", "<C-j>", ":m .+1<CR>==", { desc = "Move line down", silent = true })
 
