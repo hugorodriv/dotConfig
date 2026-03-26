0b93aa4c5313fc60d1d822cd77281e021b85bca2
Hugo Rodriguez <114680022+hugorodriv@users.noreply.github.com>
2026-03-26 11:36:08 +0100

    move lines with opt j/k

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,5 +2,5 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- Override LazyVim's default <C-j>/<C-k> window navigation with line move mappings.
+-- Use Option-j/k for moving lines so Ctrl-j/k remain available for window navigation.
 require("keymaps.move-line-ctrl-j-k")
--- nvim/lua/keymaps/move-line-ctrl-j-k.lua
+++ nvim/lua/keymaps/move-line-ctrl-j-k.lua
@@ -1,5 +1,5 @@
-vim.keymap.set("n", "<C-k>", ":m .-2<CR>==", { desc = "Move line up", silent = true })
-vim.keymap.set("n", "<C-j>", ":m .+1<CR>==", { desc = "Move line down", silent = true })
+vim.keymap.set("n", "<M-k>", ":m .-2<CR>==", { desc = "Move line up", silent = true })
+vim.keymap.set("n", "<M-j>", ":m .+1<CR>==", { desc = "Move line down", silent = true })
 
-vim.keymap.set("v", "<C-k>", ":m '<-2<CR>gv=gv", { desc = "Move selection up", silent = true })
-vim.keymap.set("v", "<C-j>", ":m '>+1<CR>gv=gv", { desc = "Move selection down", silent = true })
+vim.keymap.set("v", "<M-k>", ":m '<-2<CR>gv=gv", { desc = "Move selection up", silent = true })
+vim.keymap.set("v", "<M-j>", ":m '>+1<CR>gv=gv", { desc = "Move selection down", silent = true })
